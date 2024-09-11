6f83c5ddf13ce72e5131cca7e11aadde43e8b599
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2024-09-12 01:54:12 +0900

    [feat] 8-8 ListMap

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,24 +1,16 @@
 package hello.core;
 
-import hello.core.discount.DiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.member.MemoryMemberRepository;
-import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
-import org.springframework.context.annotation.Bean;
+
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
-
-@Configuration //설정정보
+import static org.springframework.context.annotation.ComponentScan.*;
+@Configuration
 @ComponentScan(
-        // 탐색할 패키지 지정
-        basePackages = "hello.core.member",
-        // 제외 필터
-        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
-)
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes =
+                Configuration.class))
 public class AutoAppConfig {
 
 }
+
